58ac9f565d1467df0c7cabbe0385bfd12f4639a4
Thomas-Gambin <t.gambin.pro@gmail.com>
2024-07-02 12:17:26 +0200

    UserId fait

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\UserRepository;
+use App\Repository\ValidateOrderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/{id}', name: 'app_user_index', methods: ['GET'])]
+    public function user(int $id, UserRepository $userRepository, ValidateOrderRepository $validateOrderRepository): Response
+    {
+        $user = $userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $validateOrders = $validateOrderRepository->findAll();
+
+        $chooseThemeId = $user->getChooseTheme() ? $user->getChooseTheme()->getId() : null;
+
+        // Transform the user into an array
+        $userData = [
+            'id' => $user->getId(),
+            'choose_theme_id' =>$chooseThemeId,
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'nickname' => $user->getNickname(),
+            'picture' => $user->getPicture(),
+            'email' => $user->getEmail(),
+        ];
+
+        // Return the data in JSON
+        return $this->json([
+            'user' => $userData,
+            'validateOrders' => $validateOrders,
+        ]);
+    }
+}
