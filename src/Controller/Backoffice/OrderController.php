b942489bbda8751b8c5dfd733244fa43ee36e9f3
guillaumecharnier <guillaume.charnier@oclock.school>
2024-07-09 15:42:14 +0200

    backoffice crud ok

---
+++ src/Controller/Backoffice/OrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Backoffice;
+
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('back/order')]
+class OrderController extends AbstractController
+{
+    #[Route('/', name: 'app_order_index', methods: ['GET'])]
+    public function index(OrderRepository $orderRepository): Response
+    {
+        return $this->render('backoffice/order/index.html.twig', [
+            'orders' => $orderRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_order_show', methods: ['GET'])]
+    public function show(Order $order): Response
+    {
+        return $this->render('backoffice/order/show.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_order_delete', methods: ['POST'])]
+    public function delete(Request $request, Order $order, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($order);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_order_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
