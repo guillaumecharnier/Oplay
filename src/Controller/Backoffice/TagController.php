82fdbf609ca0085d6f9ec5bbb5caa528e27d9395
guillaumecharnier <guillaume.charnier@oclock.school>
2024-07-09 17:16:01 +0200

    backoffice game ok + styled all views consistently

--- src/Controller/Backoffice/TagController.php
+++ src/Controller/Backoffice/TagController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/tag')]
+#[Route('back/tag')]
 class TagController extends AbstractController
 {
     #[Route('/', name: 'app_back_tag_index', methods: ['GET'])]
