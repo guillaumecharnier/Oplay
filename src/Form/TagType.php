6415f5b548f19692e0d113e771f30406024a5342
Thomas-Gambin <t.gambin.pro@gmail.com>
2024-07-10 14:04:37 +0200

    correction bug

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -2,11 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Game;
 use App\Entity\Tag;
-use App\Entity\User;
-use Faker\Provider\ar_EG\Text;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
