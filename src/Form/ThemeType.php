3326e0f077bb1aef5aaeea49cb816dc28f355a7e
Thomas-Gambin <t.gambin.pro@gmail.com>
2024-07-10 14:00:15 +0200

    add constraint in tag,user and theme form

--- src/Form/ThemeType.php
+++ src/Form/ThemeType.php
@@ -4,16 +4,24 @@ namespace App\Form;
 
 use App\Entity\Theme;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ThemeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name', TypeTextType::class, [
+                'label' => 'Name',
+                'constraints' => [
+                    new NotNull(),
+                    new Length(['min' => 5, 'max' => 25]),
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
