07fb5c2f1d216c503233a9d07c31afacb8ac0afd
guillaumecharnier <guillaume.charnier@oclock.school>
2024-07-18 11:30:42 +0200

    security in backoffice ok

--- src/Controller/Backoffice/MainController.php
+++ src/Controller/Backoffice/MainController.php
@@ -5,15 +5,15 @@ namespace App\Controller\Backoffice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main_home', methods: 'GET')]
     public function home(): Response
     {
-        // Vérifier si l'utilisateur a le rôle requis (par exemple ROLE_ADMIN) avant d'autoriser l'accès
         if (!$this->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Accès refusé.');
+            throw new AccessDeniedException('Accès refusé.');
         }
         
         return $this->render('backoffice/main/main.html.twig');
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,39 +10,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    // #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
-    // public function login(AuthenticationUtils $authenticationUtils, Request $request): JsonResponse
-    // {
-    //     // Obtenir l'erreur de connexion s'il y en a une
-    //     $error = $authenticationUtils->getLastAuthenticationError();
+     #[Route(path: '/login', name: 'app_login', methods: ['GET', 'POST'])]
+     public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+     {
+         // Obtenir l'erreur de connexion s'il y en a une
+         $error = $authenticationUtils->getLastAuthenticationError();
 
-    //     // Dernier nom d'utilisateur saisi par l'utilisateur
-    //     //$lastUsername = $authenticationUtils->getLastUsername();
+         // Dernier nom d'utilisateur saisi par l'utilisateur
+         $lastUsername = $authenticationUtils->getLastUsername();
 
-    //     // Décoder les données JSON de la requête
-    //     $data = json_decode($request->getContent(), true);
+         // Rendre le template du formulaire de connexion
+         return $this->render('security/login.html.twig', [
+             'last_username' => $lastUsername,
+             'error' => $error,
+         ]);
+     }
 
-    //     // Vérifier si les données JSON sont valides
-    //     if (empty($data['username']) || empty($data['password'])) {
-    //         return new JsonResponse(['error' => 'Invalid username or password'], Response::HTTP_BAD_REQUEST);
-    //     }
-
-    //     // Déterminer le code de statut basé sur la présence d'une erreur
-    //     $statusCode = $error ? Response::HTTP_BAD_REQUEST : Response::HTTP_OK;
-
-    //     // Créer un tableau de données à renvoyer en JSON
-    //     $responseData = [
-    //         //'last_username' => $lastUsername,
-    //         'error' => $error ? $error->getMessage() : null,
-    //     ];
-
-    //     // Renvoyer une réponse JSON avec le statut HTTP approprié
-    //     return new JsonResponse($responseData, $statusCode);
-    // }
-
-    // #[Route(path: '/logout', name: 'app_logout', methods: ['POST'])]
-    // public function logout(): void
-    // {
-    //     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    // }
+     #[Route(path: '/logout', name: 'app_logout', methods: ['POST'])]
+     public function logout(): void
+     {
+         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+     }
 }
