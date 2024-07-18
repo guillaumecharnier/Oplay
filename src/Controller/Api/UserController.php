accbaa8d7930e22f339cbb067eee99a0872bc005
guillaumecharnier <guillaume.charnier@oclock.school>
2024-07-18 14:00:12 +0200

    new api route for register

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -62,59 +62,6 @@ class UserController extends AbstractController
         return $this->json($user, Response::HTTP_OK, [], ['groups' => 'user_show']);
     }
 
-    #[Route('/register', name: 'register', methods: ['POST'])]
-    public function register(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        ValidatorInterface $validator,
-        UserPasswordHasherInterface $passwordHasher,
-    ): JsonResponse {
-        // Prendre les données JSON de la requête
-        $data = json_decode($request->getContent(), true);
-
-        if (!$data) {
-            return new JsonResponse(['error' => 'Invalid JSON'], 400);
-        }
-
-        // Créer le nouvel utilisateur
-        $user = new User();
-        $user->setFirstname($data['firstname']);
-        $user->setLastname($data['lastname']);
-        $user->setNickname($data['nickname']);
-        $user->setEmail($data['email']);
-        $user->setRoles(['ROLE_USER']);
-
-        // Télécharger l'image de profil si l'utilisateur a défini une image
-        if (!empty($data['picture'])) {
-            $pictureFile = base64_decode($data['picture']);
-            if ($pictureFile) {
-                $pictureFileName = md5(uniqid()) . '.jpg' . '.png'; 
-                file_put_contents($this->getParameter('pictures_directory') . '/' . $pictureFileName, $pictureFile);
-                $user->setPicture($pictureFileName);
-            } else {
-                return new JsonResponse(['error' => 'Invalid picture format'], 400);
-            }
-        }
-
-        // Hasher le mot de passe
-        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
-        $user->setPassword($hashedPassword);
-
-        // Valider les données utilisateur
-        $errors = $validator->validate($user);
-        if (count($errors) > 0) {
-            $errorsString = (string) $errors;
-
-            return new JsonResponse(['error' => $errorsString], 400);
-        }
-
-        // Enregistrer les données utilisateur
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return new JsonResponse(['message' => 'Utilisateur créé avec succès'], 201);
-    }
-
     #[Route('/edit', name: 'edit', methods: ['PATCH'])]
     public function edit(
         Request $request,
